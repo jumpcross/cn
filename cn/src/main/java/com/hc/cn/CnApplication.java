930f08581f07b0410f741bbc60a091272f0ab4dd
Charlie <2279993496@qq.com>
2017-07-12 23:30:54 +0800

    First

---
+++ cn/src/main/java/com/hc/cn/CnApplication.java
@@ -0,0 +1,26 @@
+package com.hc.cn;
+
+import com.hc.cn.storage.StorageProperties;
+import com.hc.cn.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class CnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnApplication.class, args);
+
+	}
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
